b853bd9e8e695b5439c62796aee88b01e853ea63
David-Voss <124778797+David-Voss@users.noreply.github.com>
2025-01-09 16:17:30 +0100

    Added a menu bar to TextEditorGUI() with two menus. First menu: fileMenu, with the items newItem and saveItem. Second menu: editMenu, with the items undoItem and searchItem.

--- src/gui/TextEditorGUI.java
+++ src/gui/TextEditorGUI.java
@@ -5,13 +5,44 @@ import java.awt.*;
 
 public class TextEditorGUI {
 
-    JFrame frame = new JFrame("Texteditor");
+    JFrame frame;
 
     public TextEditorGUI() {
+        frame = new JFrame("Texteditor");
         frame.setLayout(new BorderLayout());
         frame.setSize(800,600);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Creating the menu bar
+        JMenuBar menuBar =new JMenuBar();
+
+        // Creating the file menu
+        JMenu fileMenu = new JMenu("Datei");
+        menuBar.add(fileMenu);
+
+        JMenuItem newItem = new JMenuItem("Neu");
+        fileMenu.add(newItem);
+
+        JMenuItem saveItem = new JMenuItem("Speichern");
+        fileMenu.add(saveItem);
+
+        // Creating the edit menu
+        JMenu editMenu = new JMenu("Bearbeiten");
+        menuBar.add(editMenu);
+
+        JMenuItem undoItem = new JMenuItem("Rückgängig");
+        editMenu.add(undoItem);
+
+        JMenuItem searchItem = new JMenuItem("Suchen");
+        editMenu.add(searchItem);
+
+        // Adding the meu bar to JFrame
+        frame.setJMenuBar(menuBar);
+
         frame.setVisible(true);
     }
+
+
+
 }
