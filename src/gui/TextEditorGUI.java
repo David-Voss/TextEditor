1dd68d916d765f67cc90315a7a848e79a6a6c850
David-Voss <124778797+David-Voss@users.noreply.github.com>
2025-01-09 15:11:40 +0100

    Created the constructor TextEditorGUI() in class TextEditorGUI which opens a window on programm start. Class successfully called via TextEditorMain.

--- src/gui/TextEditorGUI.java
+++ src/gui/TextEditorGUI.java
@@ -1,4 +1,17 @@
 package gui;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class TextEditorGUI {
+
+    JFrame frame = new JFrame("Texteditor");
+
+    public TextEditorGUI() {
+        frame.setLayout(new BorderLayout());
+        frame.setSize(800,600);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
